ba6933c24750cd79c7972fcbc549ff46787add73
alolf <alolfjay@163.com>
2018-08-14 15:06:22 +0800

    first commit

---
+++ Broadcast/app/src/main/java/com/alolf/broadcast/MyBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.alolf.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    public MyBroadcastReceiver(){}
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context,"a",Toast.LENGTH_SHORT).show();
+    }
+}
